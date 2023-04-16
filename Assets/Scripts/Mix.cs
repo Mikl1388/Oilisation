6437b38d5d8f426eceb74984861ef5c5ac909fba
Mikl1388 <mikl1388@outlook.com>
2023-04-16 14:35:31 +0700

    changed Amount to Volume

--- Assets/Scripts/Mix.cs
+++ Assets/Scripts/Mix.cs
@@ -7,5 +7,5 @@ public class Mix : MonoBehaviour
     public Water Water { get; private set; }
     public Oil Oil { get; private set; }
     public Gas Gas { get; private set; }
-    public uint Amount { get { return Water.Amount + Oil.Amount + Gas.Amount; } }
+    public uint Volume { get { return Water.Volume + Oil.Volume + Gas.Volume; } }
 }
